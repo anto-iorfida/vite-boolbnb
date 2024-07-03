d535943695a0c2f17c45f31878631b881d3048d4
DavideTessari <davtex@gmail.com>
2024-07-03 15:50:04 +0200

    fix card apartamet list

--- src/components/SingleApartment.vue
+++ src/components/SingleApartment.vue
@@ -26,15 +26,16 @@ export default {
 
 <template>
     <div class="col">
-        <div v-if="!loading" class="card my-3" style="width: 100%;">
-            <div class="card-body">
+        <div v-if="!loading" class="card my-3 h-100" style="width: 100%;">
+            <div class="card-body d-flex flex-column">
                 <h4 class="card-title">{{ apartmentInfo.title }}</h4>
-                <img :src="apartmentInfo.thumb" alt="Thumbnail" style="max-width: 100%; height: auto;">
-                <router-link :to="{ name: 'details-apartment', params: { slug: apartmentInfo.slug } }"
-                    class="btn btn-primary">Info</router-link>
+                <img :src="apartmentInfo.thumb" alt="Thumbnail" class="img-fluid mb-3" style="max-width: 100%; height: auto;">
+                <div class="mt-auto">
+                    <router-link :to="{ name: 'details-apartment', params: { slug: apartmentInfo.slug } }"
+                        class="btn btn-primary w-100">Info</router-link>
+                </div>
             </div>
         </div>
-
         <Loader v-else />
     </div>
 </template>
@@ -43,5 +44,9 @@ export default {
 /* Stili specifici per il componente se necessario */
 .card {
     margin: 0 auto; /* Center the card horizontally */
+}
+.img-fluid {
+    max-height: 200px;
+    object-fit: cover;
 }
 </style>
