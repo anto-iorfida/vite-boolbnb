a65901444fa8618be23030e98fdc927954fed77a
DavideTessari <davtex@gmail.com>
2024-07-04 09:25:17 +0200

    fix

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -34,9 +34,9 @@ export default {
                 <li class="nav-item">
                     <a class="nav-link" href="#">Contact</a>
                 </li>
-                <li class="nav-item">
+                <!-- <li class="nav-item">
                     <a class="btn btn-primary" href="#">Book Now</a>
-                </li>
+                </li> -->
             </ul>
         </div>
     </div>
