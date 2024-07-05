e882c43cf87c2272c4b18168f4bfc7b011329379
Casimiro-Moliterni <moliternicasimiro@hotmail.it>
2024-07-05 19:20:50 +0200

    add carousel details

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,101 +1,3 @@
-<!-- <script>
-import axios from 'axios';
-
-export default {
-    name: "SearchBar",
-    data() {
-        return {
-            latitude: '',
-            longitude: '',
-            query: '',
-            suggestions: [],
-            radius: '' // Raggio di ricerca in km
-        }
-    },
-    methods: {
-        // Funzione asincrona per cercare suggerimenti di indirizzo
-        async searchAddress() {
-            // Verifica se la lunghezza della query è maggiore di 1
-            if (this.query.length > 1) {
-                // Chiave API per accedere al servizio TomTom
-                const apiKey = 'tNdeH4PSEGzxLQ1CKK0HdCagLd1BsXSc';
-                // Costruzione dell'URL per la ricerca degli indirizzi tramite TomTom API
-                const url = `https://api.tomtom.com/search/2/search/${this.query}.json?key=${apiKey}&countrySet=IT`;
-                try {
-                    // Effettua una richiesta GET asincrona all'API TomTom
-                    const response = await axios.get(url);
-                    // Assegna i risultati della ricerca agli suggerimenti
-                    this.suggestions = response.data.results;
-                } catch (error) {
-                    // Gestisce gli errori durante il fetch dei suggerimenti
-                    console.error('Errore durante il fetch dei suggerimenti:', error);
-                }
-            } else {
-                // Se la lunghezza della query non è maggiore di 1, svuota gli suggerimenti
-                this.suggestions = [];
-            }
-        },
-
-        // Funzione per selezionare un suggerimento di indirizzo
-        selectSuggestion(suggestion) {
-            // Imposta la latitudine basata sulla posizione del suggerimento
-            this.latitude = suggestion.position.lat;
-            // Imposta la longitudine basata sulla posizione del suggerimento
-            this.longitude = suggestion.position.lon;
-            // Imposta la query con l'indirizzo libero del suggerimento
-            this.query = suggestion.address.freeformAddress;
-            // Svuota l'array dei suggerimenti
-            this.suggestions = [];
-        },
-
-        // Funzione per ottenere la ricerca degli appartamenti
-        getSearchApartment() {
-            // Reindirizza alla route "apartments"
-            this.$router.push({ name: 'apartments' });
-            // Emette un evento con tutti i parametri di ricerca
-            this.$emit('search-apartments', {
-                latitude: this.latitude,
-                longitude: this.longitude,
-                radius: this.radius
-            });
-        }
-    }
-
-}
-</script>
-
-<template>
-    <div class="text-center search-bar-container text-cente p-4 mt-5 mx-auto custom rounded-4">
-        <label class="form-label"><strong>Cerca il tuo appartamento</strong></label>
-        <input type="text" class="form-control" id="address" name="address" v-model="query" @input="searchAddress"
-            autocomplete="off" />
-
-        <ul v-if="suggestions.length" class="list-group mt-2">
-            <li v-for="(suggestion, index) in suggestions" :key="index" class="list-group-item list-group-item-action"
-                @click="selectSuggestion(suggestion)">
-                {{ suggestion.address.freeformAddress }}
-            </li>
-            
-        </ul>
-
-        <label for="radius" class="form-label mt-2">Raggio (km)</label>
-        <input type="number" class="form-control" id="radius" v-model.number="radius" />
-
-        // Pulsante di ricerca 
-        <button class="btn btn-primary mt-3" @click="getSearchApartment()">Cerca</button>
-        // <router-link class="btn btn-primary mt-3" @click="getSearchApartment()" :to="{ name: 'apartments' }">Cerca</router-link> 
-    </div>
-</template>
-
-<style lang="scss" scoped>
-.list-group-item-action {
-    cursor: pointer;
-}
-.custom {
-    max-width: 70%;
-}
-</style> -->
-
 <script>
 import axios from 'axios';
 
@@ -107,7 +9,8 @@ export default {
             longitude: '',
             query: '',
             suggestions: [],
-            radius: '' // Raggio di ricerca in km
+            radius: '', // Raggio di ricerca in km,
+            active: true
         }
     },
     methods: {
@@ -146,6 +49,12 @@ export default {
                     radius: this.radius
                 }
             });
+        },
+
+        // funzione per i filitri di ricerca
+
+        getFilterServices() {
+            this.active = false
         }
     }
 }
@@ -153,18 +62,39 @@ export default {
 
 <template>
     <div class="text-center search-bar-container text-cente p-4 mt-5 mx-auto custom rounded-4">
-        <label class="form-label"><strong>Cerca il tuo appartamento</strong></label>
-        <input type="text" class="form-control" id="address" name="address" v-model="query" @input="searchAddress" autocomplete="off" />
+        <div class="d-flex align-items-center gap-3">
+            <div class="w-50">
+                <label class="form-label"><strong>Cerca il tuo appartamento</strong></label>
+                <input type="text" class="form-control position-relative" id="address" name="address" v-model="query"
+                    @input="searchAddress" autocomplete="off" />
 
-        <ul v-if="suggestions.length" class="list-group mt-2">
-            <li v-for="(suggestion, index) in suggestions" :key="index" class="list-group-item list-group-item-action" @click="selectSuggestion(suggestion)">
-                {{ suggestion.address.freeformAddress }}
+                <ul v-if="suggestions.length" class="list-group mt-2 mb-5 position-absolute">
+                    <li v-for="(suggestion, index) in suggestions" :key="index"
+                        class="list-group-item list-group-item-action" @click="selectSuggestion(suggestion)">
+                        {{ suggestion.address.freeformAddress }}
+                    </li>
+                </ul>
+            </div>
+
+            <div class="w-50">
+                <label for="radius" class="form-label "><strong>Raggio (km)</strong></label>
+                <input type="number" class="form-control" id="radius" v-model.number="radius" />
+            </div>
+        </div>
+        <!-- filtri  -->
+        <div>
+            <div @click=" getFilterServices()"
+                class="d-flex align-items-center justify-content-center gap-2 border w-25 mt-2 rounded-3 p-2 text-black btn">
+                <i class="fa-solid fa-list"></i>
+                <div>Filtri</div>
+            </div>
+        </div>
+        <ul class="my-ul-list" v-if="active == false">
+            <li>
+                <label for="check">prova</label>
+                <input id="check" type="checkbox">
             </li>
         </ul>
-
-        <label for="radius" class="form-label mt-2">Raggio (km)</label>
-        <input type="number" class="form-control" id="radius" v-model.number="radius" />
-
         <button class="btn btn-primary mt-3 w-100" @click="getSearchApartment()">Cerca</button>
     </div>
 </template>
@@ -173,6 +103,7 @@ export default {
 .list-group-item-action {
     cursor: pointer;
 }
+
 .custom {
     max-width: 70%;
     background: #ffffff2a;
@@ -181,6 +112,7 @@ export default {
     border: none;
 }
 
+ul {
+    list-style: none;
+}
 </style>
-
-
--- src/pages/DetailsApartment.vue
+++ src/pages/DetailsApartment.vue
@@ -71,7 +71,39 @@ export default {
                 </div>
                 <div class="card-body">
                     <div class="row my-img-wrapper ">
-                        <div v-if="apartment.thumb" class="my-img-wrapper col-12 col-lg-7 mt-1 mb-3">
+                        <div id="carouselExampleIndicators" class="carousel slide d-block d-lg-none">
+                            <div class="carousel-indicators">
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0"
+                                    class="active" aria-current="true" aria-label="Slide 1"></button>
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+                                    aria-label="Slide 2"></button>
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+                                    aria-label="Slide 3"></button>
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3"
+                                    aria-label="Slide 4"></button>
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4"
+                                    aria-label="Slide 5"></button>
+                            </div>
+                            <div class="carousel-inner rounded-3">
+                                <div class="carousel-item active rounded-3">
+                                    <img :src="apartment.thumb" class="d-block w-100 rounded-3" alt="...">
+                                </div>
+                                <div class="carousel-item rounded-3" v-for="singleimage in apartment.albums">
+                                    <img :src="singleimage.image" class="d-block w-100 rounded-3" alt="...">
+                                </div>
+                            </div>
+                            <button class="carousel-control-prev " type="button"
+                                data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon my-icon-previous" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button"
+                                data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                                <span class="carousel-control-next-icon my-icon-previous" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                        </div>
+                        <div v-if="apartment.thumb" class="my-img-wrapper d-none d-lg-flex col-12 col-lg-7 mt-1 mb-3">
                             <img :src="apartment.thumb" alt="thumb" class="rounded-4">
                         </div>
                         <div class="col-5 d-none d-lg-flex">
@@ -85,9 +117,9 @@ export default {
                     <blockquote class="blockquote mt-4 w-50 border-bottom pb-3">
                         <p class="mb-0"><strong>{{ apartment.address }}</strong> </p>
                         <div class="mb-4 fs-6">
-                            <span><small>{{ apartment.number_beds}} letti·</small></span>
-                            <span><small>{{ apartment.number_rooms}} camere da letto ·</small></span>
-                            <span><small>{{ apartment.number_baths}} bagni</small></span>
+                            <span><small>{{ apartment.number_beds }} letti·</small></span>
+                            <span><small>{{ apartment.number_rooms }} camere da letto ·</small></span>
+                            <span><small>{{ apartment.number_baths }} bagni</small></span>
                         </div>
                         <div class="d-flex align-items-start gap-3 border-bottom pb-3">
                             <div
@@ -101,19 +133,20 @@ export default {
                                 <div class="fs-6 fw-normal">4 anni di esperienza</div>
                             </div>
                         </div>
-                        <div  class="border-bottom pb-4">
+                        <div class="border-bottom pb-4">
                             <h5 class="pt-2">Servizi offerti dall'host</h5>
-                            <div v-for="singleservice in apartment.services"  class="mt-3 d-flex gap-2 align-items-center fs-6 fw-semibold ">
-                            <i :class="singleservice.icon"></i>
-                            <div>{{ singleservice.name }}</div>
-                        </div>
+                            <div v-for="singleservice in apartment.services"
+                                class="mt-3 d-flex gap-2 align-items-center fs-6 fw-semibold ">
+                                <i :class="singleservice.icon"></i>
+                                <div>{{ singleservice.name }}</div>
+                            </div>
                         </div>
                         <p v-if="apartment.description" class="fs-6 mt-2">
                             <strong>Descrizione:</strong> {{ apartment.description }}
                         </p>
-                        <p class="fs-6"><strong >Indirizzo:</strong> {{ apartment.address }}</p>
-                        <h6 v-if=" apartment.visibility == 0">Appartamento non disponibile</h6>
-                        <h6 v-if=" apartment.visibility == 1">Appartamento disponibile </h6>
+                        <p class="fs-6"><strong>Indirizzo:</strong> {{ apartment.address }}</p>
+                        <h6 v-if="apartment.visibility == 0">Appartamento non disponibile</h6>
+                        <h6 v-if="apartment.visibility == 1">Appartamento disponibile </h6>
                     </blockquote>
                     <div>
                         <h5>Questi sono le caratteristiche complete dell'appartamento :</h5>
@@ -127,7 +160,7 @@ export default {
                                         <div>
                                             bagni
                                         </div>
-                                        {{ apartment.number_baths}}
+                                        {{ apartment.number_baths }}
                                     </div>
                                 </div>
                             </div>
@@ -138,35 +171,35 @@ export default {
                                             <i class="fa-solid fa-bed"></i>
                                         </div>
                                         <div>
-                                          letti
+                                            letti
                                         </div>
                                         {{ apartment.number_beds }}
                                     </div>
                                 </div>
                             </div>
-                            <div class="col"  v-if="apartment.number_rooms">
-                                <div class="card text-center p-1 " >
+                            <div class="col" v-if="apartment.number_rooms">
+                                <div class="card text-center p-1 ">
                                     <div>
-                                        <div >
+                                        <div>
                                             <i class="fa-solid fa-house"></i>
                                         </div>
                                         <div>
                                             camere
                                         </div>
-                                        {{ apartment.number_rooms}}
+                                        {{ apartment.number_rooms }}
                                     </div>
                                 </div>
                             </div>
                             <div class="col " v-if="apartment.square_meters">
                                 <div class="card text-center p-1 ">
                                     <div>
-                                        <div >
+                                        <div>
                                             <i class="fa-solid fa-ruler-combined"></i>
                                         </div>
                                         <div>
-                                           MetriQuadri
+                                            MetriQuadri
                                         </div>
-                                        {{ apartment.square_meters}}
+                                        {{ apartment.square_meters }}
                                     </div>
                                 </div>
                             </div>
@@ -212,5 +245,16 @@ export default {
     height: 500px;
     width: 100%;
     object-fit: cover;
+}
+.my-icon-previous{
+    border-radius: 100%;
+    background-color: rgba($color: #000000, $alpha: 0.7);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.carousel img{
+    height: 400px;
 }
 </style>
