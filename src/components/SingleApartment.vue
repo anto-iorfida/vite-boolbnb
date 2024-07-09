e4fda9a06a674e06ae05021d042647292a4d7387
anto-iorfida <antonioiorfida@hotmail.com>
2024-07-09 12:12:23 +0200

    fix service

--- src/components/SingleApartment.vue
+++ src/components/SingleApartment.vue
@@ -38,23 +38,23 @@ export default {
                 <div :id="'carousel-' + apartmentInfo.id" class="carousel slide" data-bs-ride="carousel">
                     <div class="carousel-indicators">
                         <button v-for="(image, index) in apartmentInfo.albums" :key="index"
-                                :data-bs-target="'#carousel-' + apartmentInfo.id" :data-bs-slide-to="index"
-                                :class="{ active: index === 0 }"></button>
+                            :data-bs-target="'#carousel-' + apartmentInfo.id" :data-bs-slide-to="index"
+                            :class="{ active: index === 0 }"></button>
                     </div>
                     <div class="carousel-inner">
                         <div v-for="(image, index) in apartmentInfo.albums" :key="index"
-                             :class="{ 'carousel-item': true, active: index === 0 }">
-                            <img :src="index === 0 ? apartmentInfo.thumb : image.image" class="wrapper-img d-block w-100 rounded-3"
-                                 :alt="'Slide ' + (index + 1)">
+                            :class="{ 'carousel-item': true, active: index === 0 }">
+                            <img :src="index === 0 ? apartmentInfo.thumb : image.image"
+                                class="wrapper-img d-block w-100 rounded-3" :alt="'Slide ' + (index + 1)">
                         </div>
                     </div>
                     <button class="carousel-control-prev" type="button"
-                            :data-bs-target="'#carousel-' + apartmentInfo.id" data-bs-slide="prev" @click="prevSlide">
+                        :data-bs-target="'#carousel-' + apartmentInfo.id" data-bs-slide="prev" @click="prevSlide">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Previous</span>
                     </button>
                     <button class="carousel-control-next" type="button"
-                            :data-bs-target="'#carousel-' + apartmentInfo.id" data-bs-slide="next" @click="nextSlide">
+                        :data-bs-target="'#carousel-' + apartmentInfo.id" data-bs-slide="next" @click="nextSlide">
                         <span class="carousel-control-next-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Next</span>
                     </button>
@@ -68,20 +68,28 @@ export default {
                 <div>
                     <small>a {{ apartmentInfo.distance }} Km</small>
                 </div>
+                <div class="border-bottom pb-4 ">
+                    <small class="pt-1">Servizi offerti </small>
+                    <div v-for="singleservice in apartmentInfo.services " :key="singleservice.id"
+                        class="mt-3 d-flex  gap-2 align-items-center fs-6 fw-semibold ">
+                        <i :class="singleservice.icon"></i>
+                    </div>
+                </div>
                 <h6 v-if="apartmentInfo.visibility == 0">Non disponibile</h6>
                 <h6 v-if="apartmentInfo.visibility == 1">Disponibile</h6>
             </div>
         </div>
-        <Loader v-else/>
+        <Loader v-else />
     </div>
     <router-link :to="{ name: 'details-apartment', params: { slug: apartmentInfo.slug } }"
-    class="mb-2 position-absolute h-100 w-100"></router-link>
+        class="mb-2 position-absolute h-100 w-100"></router-link>
 </template>
 
 <style scoped>
 /* Stili specifici per il componente se necessario */
 .card {
-    margin: 0 auto; /* Centra la card orizzontalmente */
+    margin: 0 auto;
+    /* Centra la card orizzontalmente */
 }
 
 .wrapper-img {
